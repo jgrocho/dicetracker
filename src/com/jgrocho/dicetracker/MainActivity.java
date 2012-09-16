3d22f15c2a930725fd797c8ef2a9f3088fb731da
Jonathan Grochowski <jongrocho@gmail.com>
2012-09-16 02:44:22 -0700

    Create TabListener class

--- src/com/jgrocho/dicetracker/MainActivity.java
+++ src/com/jgrocho/dicetracker/MainActivity.java
@@ -1,14 +1,48 @@
 package com.jgrocho.dicetracker;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+
+    public static class TabListener<T extends Fragment> implements
+            ActionBar.TabListener {
+        private Fragment mFragment;
+        private final Activity mActivity;
+        private final String mTag;
+        private final Class<T> mClass;
+
+        public TabListener(Activity activity, String tag, Class<T> klass) {
+            mActivity = activity;
+            mTag = tag;
+            mClass = klass;
+        }
+
+        public void onTabSelected(Tab tab, FragmentTransaction ft) {
+            if (mFragment == null) {
+                mFragment = Fragment.instantiate(mActivity, mClass.getName());
+                ft.add(android.R.id.content, mFragment, mTag);
+            } else {
+                ft.attach(mFragment);
+            }
+        }
+
+        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+            if (mFragment != null)
+                ft.detach(mFragment);
+        }
+
+        public void onTabReselected(Tab tab, FragmentTransaction ft) {
+        }
+    }
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
